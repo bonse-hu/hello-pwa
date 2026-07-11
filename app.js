c47d9fd3a039db3d3e034f96cc0d864763404ab5
bonse <uematsupiano@gmail.com>
2026-07-11 12:47:49 +0900

    Add quiz history

--- app.js
+++ app.js
@@ -1,147 +1,242 @@
 const allQuestions = [
 
-{
-question:"sinA cosB を積和公式で表せせ",
+    {
+        question: "sinA cosB を積和公式で表せせ",
 
-choices:[
-"1/2{sin(A+B)+sin(A-B)}",
-"1/2{sin(A+B)-sin(A-B)}",
-"sin(A+B)",
-"cos(A+B)"
-],
+        choices: [
+            "1/2{sin(A+B)+sin(A-B)}",
+            "1/2{sin(A+B)-sin(A-B)}",
+            "sin(A+B)",
+            "cos(A+B)"
+        ],
 
-answer:0
-},
-
-
-{
-question:"cosA cosB を積和公式で表せ",
-
-choices:[
-"1/2{cos(A+B)+cos(A-B)}",
-"1/2{sin(A+B)+sin(A-B)}",
-"cos(A+B)",
-"sin(A+B)"
-],
-
-answer:0
-},
+        answer: 0
+    },
 
 
-{
-question:"sinA sinB を積和公式で表せ",
+    {
+        question: "cosA cosB を積和公式で表せ",
 
-choices:[
-"1/2{cos(A-B)-cos(A+B)}",
-"1/2{cos(A+B)+cos(A-B)}",
-"sin(A+B)",
-"cos(A-B)"
-],
+        choices: [
+            "1/2{cos(A+B)+cos(A-B)}",
+            "1/2{sin(A+B)+sin(A-B)}",
+            "cos(A+B)",
+            "sin(A+B)"
+        ],
 
-answer:0
-},
-
-
-{
-question:"sinA+sinB の和積公式は？",
-
-choices:[
-"2sin((A+B)/2)cos((A-B)/2)",
-"2cos((A+B)/2)sin((A-B)/2)",
-"sin(A+B)",
-"cos(A+B)"
-],
-
-answer:0
-},
+        answer: 0
+    },
 
 
-{
-question:"cosA+cosB の和積公式は？",
+    {
+        question: "sinA sinB を積和公式で表せ",
 
-choices:[
-"2cos((A+B)/2)cos((A-B)/2)",
-"2sin((A+B)/2)sin((A-B)/2)",
-"cos(A+B)",
-"sin(A+B)"
-],
+        choices: [
+            "1/2{cos(A-B)-cos(A+B)}",
+            "1/2{cos(A+B)+cos(A-B)}",
+            "sin(A+B)",
+            "cos(A-B)"
+        ],
 
-answer:0
-}
+        answer: 0
+    },
+
+
+    {
+        question: "sinA+sinB の和積公式は？",
+
+        choices: [
+            "2sin((A+B)/2)cos((A-B)/2)",
+            "2cos((A+B)/2)sin((A-B)/2)",
+            "sin(A+B)",
+            "cos(A+B)"
+        ],
+
+        answer: 0
+    },
+
+
+    {
+        question: "cosA+cosB の和積公式は？",
+
+        choices: [
+            "2cos((A+B)/2)cos((A-B)/2)",
+            "2sin((A+B)/2)sin((A-B)/2)",
+            "cos(A+B)",
+            "sin(A+B)"
+        ],
+
+        answer: 0
+    }
 
 ];
 
 
 
-function shuffle(array){
+function shuffle(array) {
 
-return array.sort(
-()=>Math.random()-0.5
-);
+    return array.sort(
+        () => Math.random() - 0.5
+    );
 
 }
 
 
 const questions =
-shuffle([...allQuestions]).slice(0,5);
+    shuffle([...allQuestions]).slice(0, 5);
 
 
 
-let current=0;
+let current = 0;
 
-let score=0;
+let score = 0;
 
 // タイマー開始
 let startTime = Date.now();
 
-function formatTime(seconds){
+function formatTime(seconds) {
 
-let min =
-Math.floor(seconds / 60);
+    let min =
+        Math.floor(seconds / 60);
 
-let sec =
-seconds % 60;
+    let sec =
+        seconds % 60;
 
 
-return (
-String(min).padStart(2,"0")
-+
-"分"
-+
-String(sec).padStart(2,"0")
-+
-"秒"
-);
+    return (
+        String(min).padStart(2, "0")
+        +
+        "分"
+        +
+        String(sec).padStart(2, "0")
+        +
+        "秒"
+    );
 
 }
 
-function showQuestion(){
+function saveHistory(score, time) {
 
 
-document.activeElement.blur();
+    let history =
+        JSON.parse(
+            localStorage.getItem("quizHistory")
+        )
+        || [];
 
 
-let q=questions[current];
+    let now = new Date();
 
 
-document.getElementById("question")
-.innerHTML=
+    let record = {
 
-"第"+
-(current+1)+
-"問<br><br>"+
-q.question;
-
-
-
-let html="";
-
-
-q.choices.forEach(
-function(choice,index){
+        date:
+            now.getFullYear()
+            +
+            "/"
+            +
+            (now.getMonth() + 1)
+            +
+            "/"
+            +
+            now.getDate(),
 
 
-html+=`
+        score: score,
+
+
+        time: time
+
+    };
+
+
+    history.push(record);
+
+
+
+    localStorage.setItem(
+        "quizHistory",
+        JSON.stringify(history)
+    );
+
+
+}
+
+function showHistory() {
+
+
+    let history =
+        JSON.parse(
+            localStorage.getItem("quizHistory")
+        )
+        || [];
+
+
+    let html =
+        "<h2>履歴</h2>";
+
+
+
+    history.forEach(
+        item => {
+
+
+            html +=
+
+                `
+<div class="history">
+
+${item.date}
+
+<br>
+
+${item.score}/5 正解
+
+<br>
+
+${formatTime(item.time)}
+
+</div>
+
+`;
+
+
+        });
+
+
+    document.getElementById("result")
+        .innerHTML += html;
+
+
+}
+
+function showQuestion() {
+
+
+    document.activeElement.blur();
+
+
+    let q = questions[current];
+
+
+    document.getElementById("question")
+        .innerHTML =
+
+        "第" +
+        (current + 1) +
+        "問<br><br>" +
+        q.question;
+
+
+
+    let html = "";
+
+
+    q.choices.forEach(
+        function (choice, index) {
+
+
+            html += `
 
 <button 
 class="choice"
@@ -154,83 +249,83 @@ ${choice}
 
 `;
 
-});
+        });
 
 
-document.getElementById("choices")
-.innerHTML=html;
-
-
-}
-
-
-
-
-function answer(index){
-
-
-let buttons =
-document.querySelectorAll(".choice");
-
-
-// 2回押し防止
-
-buttons.forEach(
-button=>{
-button.disabled=true;
-});
-
-
-
-if(index===questions[current].answer){
-
-score++;
-
-document
-.getElementById("choice"+index)
-.classList.add("correct");
-
-
-}
-
-else{
-
-
-document
-.getElementById("choice"+index)
-.classList.add("wrong");
-
-
-document
-.getElementById(
-"choice"+questions[current].answer
-)
-.classList.add("correct");
+    document.getElementById("choices")
+        .innerHTML = html;
 
 
 }
 
 
 
-document.getElementById("result")
-.innerHTML=
 
-index===questions[current].answer
-
-?
-
-"正解！"
-
-:
-
-"不正解<br>正解は上の緑色です";
+function answer(index) {
 
 
+    let buttons =
+        document.querySelectorAll(".choice");
 
-document.getElementById("choices")
-.innerHTML +=
 
-`
+    // 2回押し防止
+
+    buttons.forEach(
+        button => {
+            button.disabled = true;
+        });
+
+
+
+    if (index === questions[current].answer) {
+
+        score++;
+
+        document
+            .getElementById("choice" + index)
+            .classList.add("correct");
+
+
+    }
+
+    else {
+
+
+        document
+            .getElementById("choice" + index)
+            .classList.add("wrong");
+
+
+        document
+            .getElementById(
+                "choice" + questions[current].answer
+            )
+            .classList.add("correct");
+
+
+    }
+
+
+
+    document.getElementById("result")
+        .innerHTML =
+
+        index === questions[current].answer
+
+            ?
+
+            "正解！"
+
+            :
+
+            "不正解<br>正解は上の緑色です";
+
+
+
+    document.getElementById("choices")
+        .innerHTML +=
+
+        `
 
 <button 
 class="next-button"
@@ -247,47 +342,47 @@ onclick="nextQuestion()">
 
 
 
-function nextQuestion(){
+function nextQuestion() {
 
 
-current++;
+    current++;
 
 
-document.getElementById("result")
-.innerHTML="";
+    document.getElementById("result")
+        .innerHTML = "";
 
 
-if(current < questions.length){
+    if (current < questions.length) {
 
-showQuestion();
+        showQuestion();
 
-}
+    }
 
-else{
-
-
-let endTime = Date.now();
+    else {
 
 
-let elapsed =
-Math.floor(
-(endTime - startTime) / 1000
-);
+        let endTime = Date.now();
 
 
-
-document.getElementById("question")
-.innerHTML="終了";
-
-
-document.getElementById("choices")
-.innerHTML="";
+        let elapsed =
+            Math.floor(
+                (endTime - startTime) / 1000
+            );
+        saveHistory(score, elapsed);
 
 
-document.getElementById("result")
-.innerHTML=
+        document.getElementById("question")
+            .innerHTML = "終了";
 
-`
+
+        document.getElementById("choices")
+            .innerHTML = "";
+
+
+        document.getElementById("result")
+            .innerHTML =
+
+            `
 <h2>結果</h2>
 
 ${questions.length}問中
@@ -299,8 +394,9 @@ ${score}問正解
 ${formatTime(elapsed)}
 
 `;
+showHistory();
 
-}
+    }
 
 }
 
